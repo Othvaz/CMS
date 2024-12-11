cf4ccea77ea94a66630fe6414275ac26c7171a54
Othvaz <david.tulus@student.pradita.ac.id>
2024-12-11 11:11:33 +0700

    The fourteenth commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
+        <link rel="icon" href="{{asset('assets/images/logo_cgp.jpg')}}" type="image/jpg">
+        <title>Can Ngopi</title>
         @vite(['resources/css/app.css',
         'resources/css/sb-admin-2.css',
         ])  
